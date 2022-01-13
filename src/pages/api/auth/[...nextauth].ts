ab2b515353c19551ccfd2fa50001b1c2741d03bd
LucasVidigal98 <lucasvidigal3@gmail.com>
2022-01-12 23:23:31 -0300

    FaunaDB Config

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import { query as q } from 'faunadb';
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -10,4 +13,23 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },
+  callbacks: {
+    async signIn({ user }) {
+      const { email } = user;
+      
+      await fauna.query(
+        q.Create(
+          q.Collection('users'),
+          { data: { email } }
+        )
+      ).catch(err => {
+        return false;
+      });
+
+      return true
+    },
+  }
 })
